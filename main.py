542850670a7b9aa44202d86f4f2463d4379d3e38
Rif <rif@kiamil.com>
2022-10-16 14:17:33 +0100

    fix: problem bind to port

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.get("/")
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+# Dev only: run "python main.py" and open http://localhost:8080
+app.run(host="localhost", port=8080, debug=True)
